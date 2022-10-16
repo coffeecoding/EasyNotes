2df02125ae456b0a714dd5646af3ead7be048b3c
CoffeeCoding <msdevacc@live.de>
2022-10-16 22:06:17 +0200

    refactored list item container views

--- lib/screens/items/components/notes_list.dart
+++ lib/screens/items/components/notes_list.dart
@@ -10,6 +10,7 @@ class NotesList extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ItemsCubit, ItemsState>(
         // potentially add condition to rebuild when changing a node's parent
+        // or even a single, not selected item changes (e.g. it's title)
         buildWhen: (prev, next) =>
             prev.selectedNote != next.selectedNote ||
             prev.selectedTopic != next.selectedTopic ||
@@ -25,31 +26,74 @@ class NotesList extends StatelessWidget {
               itemCount: itemCubits.length,
               itemBuilder: (context, i) {
                 final item = itemCubits[i];
-                return Container(
-                    padding: EdgeInsets.only(
-                        left: (item.getAncestorCount() - 1) * 20),
-                    decoration: BoxDecoration(
-                      color: (state.selectedNote != null &&
-                              itemCubits[i].id == state.selectedNote!.id)
-                          ? Theme.of(context).cardColor
-                          : Colors.transparent,
-                      border: Border(
-                          bottom: BorderSide(
-                              color: Theme.of(context).cardColor, width: 1)),
-                    ),
-                    child: ListTile(
-                      onTap: () => context.read<ItemsCubit>().selectNote(i),
-                      title: Row(children: [
-                        Icon(
-                            item.item_type == 0
-                                ? Icons.folder
-                                : Icons.note_outlined,
-                            color: clr),
-                        const SizedBox(width: 10),
-                        Flexible(child: Text(item.title)),
-                      ]),
-                    ));
+                return ItemContainer(
+                    color: clr,
+                    item: item,
+                    index: i,
+                    selectedNote: state.selectedNote,
+                    onTap: () => context.read<ItemsCubit>().selectChild(i));
               });
         });
+  }
+}
+
+class ItemContainer extends StatelessWidget {
+  const ItemContainer({
+    super.key,
+    required this.item,
+    this.selectedNote,
+    required this.index,
+    required this.color,
+    this.onTap,
+  });
+
+  final ItemCubit item;
+  final ItemCubit? selectedNote;
+  final Color color;
+  final int index;
+  final Function? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.only(left: (item.getAncestorCount() - 1) * 20),
+        decoration: BoxDecoration(
+          color: (selectedNote != null && item.id == selectedNote!.id)
+              ? Theme.of(context).cardColor
+              : Colors.transparent,
+          border: Border(
+              bottom: BorderSide(color: Theme.of(context).cardColor, width: 1)),
+        ),
+        child: ItemRow(item: item, color: color, onTap: onTap));
+  }
+}
+
+class ItemRow extends StatelessWidget {
+  const ItemRow(
+      {super.key, required this.item, required this.color, this.onTap});
+
+  final ItemCubit item;
+  final Color color;
+  final Function? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () => onTap,
+      title: Row(children: [
+        Icon(item.item_type == 0 ? Icons.folder : Icons.note_outlined,
+            color: color),
+        const SizedBox(width: 10),
+        Flexible(
+          child: Text(
+            item.title,
+            overflow:
+                TextOverflow.ellipsis, // remove this to line-break instead
+            softWrap: false,
+            maxLines: 1, // remove this to line-break instead
+          ),
+        ),
+      ]),
+    );
   }
 }
